35c6048eeb4c2af452242d469ab29534b3bec776
emme004 <andrea.monti24@studio.unibo.it>
2024-12-10 16:09:27 +0100

    interface ok

---
+++ 122-es2/src/main/java/it/unibo/es2/Logic.java
@@ -0,0 +1,9 @@
+package it.unibo.es2;
+public interface Logic {
+    boolean hit(int row, int col);
+
+    boolean quitRows();
+
+    boolean quitCol();
+
+}
---
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -0,0 +1,25 @@
+package it.unibo.es2;
+
+import it.unibo.es1.Logics;
+
+public class LogicsImpl implements Logic{
+
+    @Override
+    public boolean hit(int row, int col) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hit'");
+    }
+
+    @Override
+    public boolean quitRows() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quitRows'");
+    }
+
+    @Override
+    public boolean quitCol() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'quitCol'");
+    }
+
+}
